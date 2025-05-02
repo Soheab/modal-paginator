4a7c9bbedf00fe79b0611b8f5f687610113f5cbb
Soheab <33902984+Soheab@users.noreply.github.com>
2025-05-02 23:28:01 +0200

    Bump version for development

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "modal-paginator"
 copyright = "2023-2025, Soheab_"
 author = "Soheab_"
-release = "1.3.0"
+release = "1.4.0a"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
